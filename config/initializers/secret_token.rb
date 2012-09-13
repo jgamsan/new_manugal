4bb1914044a90e4bc5750f48cc74ba65fff6dbcb
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-09-13 18:50:20 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewManugal::Application.config.secret_token = '900240c0c6dec09d03e9d55e7385334c9453e3d29a28b197e18738af86d36a4a7f179c7cffb3eadf79a287062ea32623c56cb7d92df2309f77e16fcc2c833ed4'
